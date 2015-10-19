a238b467b01084829712299dc584b5a6382fc2c9
mayata <ayata.mesut@gmail.com>
2015-10-19 23:00:39 +0200

    Added skip feature for timeout. This will let skipping the effect of timeout to the percent of messages specified value by skip.

--- toxic_timeout.go
+++ toxic_timeout.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"time"
+	"math/rand"
+)
 
 // The TimeoutToxic stops any data from flowing through, and will close the connection after a timeout.
 // If the timeout is set to 0, then the connection will not be closed.
@@ -8,6 +11,9 @@ type TimeoutToxic struct {
 	Enabled bool `json:"enabled"`
 	// Times in milliseconds
 	Timeout int64 `json:"timeout"`
+	// Percent of requests that will be passed without any effect of this timeout feature.
+	// Between 0-100 . 
+	Skip int32 `json:"skip"`
 }
 
 func (t *TimeoutToxic) Name() string {
@@ -23,7 +29,22 @@ func (t *TimeoutToxic) SetEnabled(enabled bool) {
 }
 
 func (t *TimeoutToxic) Pipe(stub *ToxicStub) {
+
+
 	timeout := time.Duration(t.Timeout) * time.Millisecond
+
+	// Percent of messages that will be passed without any effect of this timeout feature.
+	// When not set, skip = 0 by default, all messages are affected as usual.
+	skip := int32(t.Skip)	
+		
+	// Do not apply this timeout to this percent of packages.
+	if(skip>0){
+		var randValue = rand.Int31n(100);
+		if((randValue+1) <= skip){
+			return
+		}
+	}
+	
 	if timeout > 0 {
 		select {
 		case <-time.After(timeout):
