46bb9443ac9fbbd8953c9a37986486a3cd9572c1
Jake Pittis <jakepittis@gmail.com>
2016-01-15 17:25:06 -0500

    factor commands out of main and add 3 new commands added create proxy, delete proxy and add toxic

--- cli/cli.go
+++ cli/cli.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"sort"
+
 	"github.com/Shopify/toxiproxy/client"
 	"github.com/codegangsta/cli"
-	"sort"
 
 	"fmt"
 	"os"
@@ -30,78 +33,172 @@ func main() {
 			Name:    "list",
 			Aliases: []string{"l", "li", "ls"},
 			Usage:   "List all proxies",
-			Action: func(c *cli.Context) {
-				proxies, err := toxiproxyClient.Proxies()
-				if err != nil {
-					fmt.Println("Failed to retrieve proxies: ", err)
-					os.Exit(1)
-				}
-
-				var proxyNames []string
-				for proxyName := range proxies {
-					proxyNames = append(proxyNames, proxyName)
-				}
-				sort.Strings(proxyNames)
-
-				fmt.Fprintf(os.Stderr, "%sListen\t\t%sUpstream\t%sName%s\n", blueColor, yellowColor, greenColor, noColor)
-				fmt.Fprintf(os.Stderr, "%s================================================================================%s\n", grayColor, noColor)
-
-				for _, proxyName := range proxyNames {
-					proxy := proxies[proxyName]
-					fmt.Printf("%s%s\t%s%s\t%s%s%s\n", blueColor, proxy.Listen, yellowColor, proxy.Upstream, enabledColor(proxy.Enabled), proxy.Name, noColor)
-				}
-			},
+			Action:  withToxi(list, toxiproxyClient),
 		},
 		{
 			Name:    "inspect",
 			Aliases: []string{"i", "ins"},
 			Usage:   "Inspect a single proxy",
-			Action: func(c *cli.Context) {
-				proxyName := c.Args().First()
-
-				proxy, err := toxiproxyClient.Proxy(proxyName)
-				if err != nil {
-					fmt.Printf("Failed to retrieve proxy %s: %s\n", proxyName, err.Error())
-					os.Exit(1)
-				}
-
-				// TODO It would be cool to include the enabled toxics in the pipe
-				// rendering --L-SC-> for latency + slow close enabled
-				fmt.Printf("%s%s%s\n", enabledColor(proxy.Enabled), proxy.Name, noColor)
-				fmt.Printf("%s%s %s---> %s%s%s\n\n", blueColor, proxy.Listen, grayColor, yellowColor, proxy.Upstream, noColor)
-
-				listToxics(proxy.ToxicsUpstream, "upstream")
-				fmt.Println()
-				listToxics(proxy.ToxicsDownstream, "downstream")
-			},
+			Action:  withToxi(inspect, toxiproxyClient),
 		},
 		{
 			Name:    "toggle",
 			Usage:   "Toggle enabled status on a proxy",
 			Aliases: []string{"tog"},
-			Action: func(c *cli.Context) {
-				proxyName := c.Args().First()
-
-				proxy, err := toxiproxyClient.Proxy(proxyName)
-				if err != nil {
-					fmt.Printf("Failed to retrieve proxy %s: %s\n", proxyName, err.Error())
-					os.Exit(1)
-				}
-
-				proxy.Enabled = !proxy.Enabled
-
-				err = proxy.Save()
-				if err != nil {
-					fmt.Printf("Failed to toggle proxy %s: %s\n", proxyName, err.Error())
-					os.Exit(1)
-				}
-
-				fmt.Printf("Proxy %s%s%s is now %s%s%s\n", enabledColor(proxy.Enabled), proxyName, noColor, enabledColor(proxy.Enabled), enabledText(proxy.Enabled), noColor)
-			},
+			Action:  withToxi(toggle, toxiproxyClient),
+		},
+		{
+			Name:    "create",
+			Usage:   "Create a new proxy",
+			Aliases: []string{"c", "new"},
+			Action:  withToxi(create, toxiproxyClient),
+		},
+		{
+			Name:    "delete",
+			Usage:   "Delete a proxy",
+			Aliases: []string{"d"},
+			Action:  withToxi(delete, toxiproxyClient),
+		},
+		{
+			Name:    "add",
+			Usage:   "Add a toxic to a proxy",
+			Aliases: []string{"a"},
+			Action:  withToxi(addToxic, toxiproxyClient),
 		},
 	}
 
 	app.Run(os.Args)
+}
+
+type toxiAction func(*cli.Context, *toxiproxy.Client)
+
+func withToxi(f toxiAction, t *toxiproxy.Client) func(*cli.Context) {
+	return func(c *cli.Context) {
+		f(c, t)
+	}
+}
+
+func list(c *cli.Context, t *toxiproxy.Client) {
+	proxies, err := t.Proxies()
+	if err != nil {
+		fmt.Println("Failed to retrieve proxies: ", err)
+		os.Exit(1)
+	}
+
+	var proxyNames []string
+	for proxyName := range proxies {
+		proxyNames = append(proxyNames, proxyName)
+	}
+	sort.Strings(proxyNames)
+
+	fmt.Fprintf(os.Stderr, "%sListen\t\t%sUpstream\t%sName%s\n", blueColor, yellowColor, greenColor, noColor)
+	fmt.Fprintf(os.Stderr, "%s================================================================================%s\n", grayColor, noColor)
+
+	for _, proxyName := range proxyNames {
+		proxy := proxies[proxyName]
+		fmt.Printf("%s%s\t%s%s\t%s%s%s\n", blueColor, proxy.Listen, yellowColor, proxy.Upstream, enabledColor(proxy.Enabled), proxy.Name, noColor)
+	}
+}
+func inspect(c *cli.Context, t *toxiproxy.Client) {
+	proxyName := c.Args().First()
+
+	proxy, err := t.Proxy(proxyName)
+	if err != nil {
+		fmt.Printf("Failed to retrieve proxy %s: %s\n", proxyName, err.Error())
+		os.Exit(1)
+	}
+
+	// TODO It would be cool to include the enabled toxics in the pipe
+	// rendering --L-SC-> for latency + slow close enabled
+	fmt.Printf("%s%s%s\n", enabledColor(proxy.Enabled), proxy.Name, noColor)
+	fmt.Printf("%s%s %s---> %s%s%s\n\n", blueColor, proxy.Listen, grayColor, yellowColor, proxy.Upstream, noColor)
+
+	listToxics(proxy.ToxicsUpstream, "upstream")
+	fmt.Println()
+	listToxics(proxy.ToxicsDownstream, "downstream")
+}
+
+func toggle(c *cli.Context, t *toxiproxy.Client) {
+	proxyName := c.Args().First()
+
+	proxy, err := t.Proxy(proxyName)
+	if err != nil {
+		fmt.Printf("Failed to retrieve proxy %s: %s\n", proxyName, err.Error())
+		os.Exit(1)
+	}
+
+	proxy.Enabled = !proxy.Enabled
+
+	err = proxy.Save()
+	if err != nil {
+		fmt.Printf("Failed to toggle proxy %s: %s\n", proxyName, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Proxy %s%s%s is now %s%s%s\n", enabledColor(proxy.Enabled), proxyName, noColor, enabledColor(proxy.Enabled), enabledText(proxy.Enabled), noColor)
+}
+
+func create(c *cli.Context, t *toxiproxy.Client) {
+	proxyName := c.Args().Get(0)
+	listen := c.Args().Get(1)
+	upstream := c.Args().Get(2)
+	p := &toxiproxy.Proxy{
+		Name:     proxyName,
+		Listen:   listen,
+		Upstream: upstream,
+		Enabled:  true,
+	}
+	p = t.NewProxy(p)
+	err := p.Create()
+	if err != nil {
+		log.Fatalf("Failed to create proxy: %s\n",
+			err.Error())
+	}
+	fmt.Printf("Created new proxy %s\n", proxyName)
+}
+
+func delete(c *cli.Context, t *toxiproxy.Client) {
+	proxyName := c.Args().First()
+	p, err := t.Proxy(proxyName)
+	if err != nil {
+		fmt.Printf("Failed to retrieve proxy %s: %s\n", proxyName, err.Error())
+		os.Exit(1)
+	}
+
+	err = p.Delete()
+	if err != nil {
+		log.Fatalf("Failed to delete proxy: %s\n",
+			err.Error())
+	}
+	fmt.Printf("Deleted proxy %s\n", proxyName)
+}
+
+func addToxic(c *cli.Context, t *toxiproxy.Client) {
+	proxyName := c.Args().Get(0)
+	toxicName := c.Args().Get(1)
+	direction := c.Args().Get(2)
+	toxicConfig := c.Args().Get(3)
+	p, err := t.Proxy(proxyName)
+	if err != nil {
+		fmt.Printf("Failed to retrieve proxy %s: %s\n", proxyName, err.Error())
+		os.Exit(1)
+	}
+	conf := parseToxicConfig(toxicConfig)
+	_, err = p.SetToxic(toxicName, direction, conf)
+	if err != nil {
+		log.Fatalf("Failed to set toxic: %s\n", err.Error())
+	}
+	fmt.Printf("Set %s %s toxic on proxy %s\n", direction,
+		toxicName, proxyName)
+}
+
+func parseToxicConfig(raw string) map[string]interface{} {
+	parsed := map[string]interface{}{}
+	err := json.Unmarshal([]byte(raw), &parsed)
+	if err != nil {
+		log.Fatal("Toxic config parsing error: %s\n", err)
+	}
+	return parsed
 }
 
 func enabledColor(enabled bool) string {
