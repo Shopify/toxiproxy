6b5d43d8757965ee925a5035d874a5bafcee6894
Jake Pittis <jakepittis@gmail.com>
2017-04-04 14:55:22 -0400

    factor out server establishment code

--- toxics/timeout_test.go
+++ toxics/timeout_test.go
@@ -1,62 +1,18 @@
 package toxics_test
 
 import (
+	"bytes"
 	"io"
 	"net"
 	"testing"
 	"time"
 
+	"github.com/Shopify/toxiproxy"
 	"github.com/Shopify/toxiproxy/testhelper"
 	"github.com/Shopify/toxiproxy/toxics"
 )
 
-func TestTimeoutToxicDoesNotCauseHang(t *testing.T) {
-	ln, err := net.Listen("tcp", "localhost:0")
-	if err != nil {
-		t.Fatal("Failed to create TCP server", err)
-	}
-	defer ln.Close()
-
-	proxy := NewTestProxy("test", ln.Addr().String())
-	proxy.Start()
-	defer proxy.Stop()
-
-	proxy.Toxics.AddToxicJson(ToxicToJson(t, "might_block", "latency", "upstream", &toxics.LatencyToxic{Latency: 10}))
-	proxy.Toxics.AddToxicJson(ToxicToJson(t, "to_delete", "timeout", "upstream", &toxics.TimeoutToxic{Timeout: 0}))
-
-	serverConnRecv := make(chan net.Conn)
-	go func() {
-		conn, err := ln.Accept()
-		if err != nil {
-			t.Error("Unable to accept TCP connection", err)
-		}
-		serverConnRecv <- conn
-	}()
-
-	conn, err := net.Dial("tcp", proxy.Listen)
-	if err != nil {
-		t.Fatal("Unable to dial TCP server", err)
-	}
-	defer conn.Close()
-
-	_ = <-serverConnRecv
-
-	_, err = conn.Write([]byte("hello"))
-	if err != nil {
-		t.Fatal("Unable to write to proxy", err)
-	}
-
-	time.Sleep(1 * time.Second) // Shitty sync waiting for latency toxi to get data.
-
-	err = testhelper.TimeoutAfter(time.Second, func() {
-		proxy.Toxics.RemoveToxic("might_block")
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestTimeoutToxicClosesConnectionOnRemove(t *testing.T) {
+func WithEstablishedProxy(t *testing.T, f func(net.Conn, net.Conn, *toxiproxy.Proxy)) {
 	ln, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatal("Failed to create TCP server", err)
@@ -85,23 +41,72 @@ func TestTimeoutToxicClosesConnectionOnRemove(t *testing.T) {
 	serverConn := <-serverConnRecv
 	defer serverConn.Close()
 
-	// Send data on connection to confirm link is established.
-	conn.Write([]byte("foobar"))
-	buf := make([]byte, 6)
-	serverConn.Read(buf)
+	writeAndReceive := func(from, to net.Conn) {
+		data := []byte("foobar")
+		_, err := from.Write(data)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	proxy.Toxics.AddToxicJson(ToxicToJson(t, "to_delete", "timeout", "upstream", &toxics.TimeoutToxic{Timeout: 0}))
+		err = testhelper.TimeoutAfter(time.Second, func() {
+			resp := make([]byte, len(data))
+			to.Read(resp)
+			if !bytes.Equal(resp, data) {
+				t.Fatalf("expected '%s' but got '%s'", string(data), string(resp))
+			}
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
 
-	proxy.Toxics.RemoveToxic("to_delete")
+	// Make sure we can send and receive data before continuing.
+	writeAndReceive(conn, serverConn)
+	writeAndReceive(serverConn, conn)
 
-	err = testhelper.TimeoutAfter(time.Second, func() {
-		buf = make([]byte, 1)
-		_, err = conn.Read(buf)
-		if err != io.EOF {
-			t.Fatal("expected EOF from closed connetion")
+	f(conn, serverConn, proxy)
+}
+
+func TestTimeoutToxicDoesNotCauseHang(t *testing.T) {
+	WithEstablishedProxy(t, func(conn, _ net.Conn, proxy *toxiproxy.Proxy) {
+		proxy.Toxics.AddToxicJson(ToxicToJson(t, "might_block", "latency", "upstream", &toxics.LatencyToxic{Latency: 10}))
+		proxy.Toxics.AddToxicJson(ToxicToJson(t, "to_delete", "timeout", "upstream", &toxics.TimeoutToxic{Timeout: 0}))
+
+		_, err := conn.Write([]byte("hello"))
+		if err != nil {
+			t.Fatal("Unable to write to proxy", err)
+		}
+
+		time.Sleep(1 * time.Second) // Shitty sync waiting for latency toxi to get data.
+
+		err = testhelper.TimeoutAfter(time.Second, func() {
+			proxy.Toxics.RemoveToxic("might_block")
+		})
+		if err != nil {
+			t.Fatal(err)
 		}
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+}
+
+func TestTimeoutToxicClosesConnectionOnRemove(t *testing.T) {
+	WithEstablishedProxy(t, func(conn, serverConn net.Conn, proxy *toxiproxy.Proxy) {
+		proxy.Toxics.AddToxicJson(ToxicToJson(t, "to_delete", "timeout", "upstream", &toxics.TimeoutToxic{Timeout: 0}))
+
+		proxy.Toxics.RemoveToxic("to_delete")
+
+		err := testhelper.TimeoutAfter(time.Second, func() {
+			buf := make([]byte, 1)
+			_, err := conn.Read(buf)
+			if err != io.EOF {
+				t.Fatal("expected EOF from closed connetion")
+			}
+			_, err = serverConn.Read(buf)
+			if err != io.EOF {
+				t.Fatal("expected EOF from closed server connetion")
+			}
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 }
