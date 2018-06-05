163102073d45cd3102747d744d0365dd2db1367e
Jacob Wirth <xthexder@live.com>
2018-06-05 17:54:51 -0400

    Revert "Add a failing test to make sure it works"
    This reverts commit bba62eafacc592be2b61de74ae854666f4c6bd3c.

--- link_test.go
+++ link_test.go
@@ -223,7 +223,3 @@ func TestStateCreated(t *testing.T) {
 		t.Fatalf("New toxic did not have state object created.")
 	}
 }
-
-func TestFailingTest(t *testing.T) {
-	t.Fatal("This is a failing test.")
-}
