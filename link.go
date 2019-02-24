85e4be66e6b38da6e5c8ce682a5dc102bccbef1b
Raamnath Mani <raamnathmani@gmail.com>
2019-02-24 20:06:39 +0000

    Introduce Reset peer toxic.

--- link.go
+++ link.go
@@ -2,10 +2,12 @@ package toxiproxy
 
 import (
 	"io"
+	"net"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/Shopify/toxiproxy/v2/stream"
 	"github.com/Shopify/toxiproxy/v2/toxics"
-	"github.com/sirupsen/logrus"
 )
 
 // ToxicLinks are single direction pipelines that connects an input and output via
@@ -62,6 +64,7 @@ func NewToxicLink(
 
 // Start the link with the specified toxics.
 func (link *ToxicLink) Start(name string, source io.Reader, dest io.WriteCloser) {
+
 	go func() {
 		bytes, err := io.Copy(link.input, source)
 		if err != nil {
@@ -76,6 +79,24 @@ func (link *ToxicLink) Start(name string, source io.Reader, dest io.WriteCloser)
 	for i, toxic := range link.toxics.chain[link.direction] {
 		if stateful, ok := toxic.Toxic.(toxics.StatefulToxic); ok {
 			link.stubs[i].State = stateful.NewState()
+		}
+		if _, ok := toxic.Toxic.(*toxics.ResetToxic); ok {
+			if err := source.(*net.TCPConn).SetLinger(0); err != nil {
+				logrus.WithFields(logrus.Fields{
+					"name":  link.proxy.Name,
+					"toxic": toxic.Type,
+					"err":   err,
+				}).Error("source: Unable to  setLinger(ms)")
+
+			}
+			if err := dest.(*net.TCPConn).SetLinger(0); err != nil {
+				logrus.WithFields(logrus.Fields{
+					"name":  link.proxy.Name,
+					"toxic": toxic.Type,
+					"err":   err,
+				}).Error("dest: Unable to  setLinger(ms)")
+
+			}
 		}
 
 		go link.stubs[i].Run(toxic)
