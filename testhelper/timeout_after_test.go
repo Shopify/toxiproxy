c9920ace0e11a4237bd6f08bd688429ad94edb37
Michael Nikitochkin <michael.nikitochkin@shopify.com>
2021-10-22 15:20:32 +0200

    Extract common test helpers to own files
    Proxy test file is overused of multiple logics.
    Make it a bit cleaner, moved `WithTCPServer` to testhelper.
    Tried to move `WithTCPProxy`, but current structure does not allow it.
    If move it under package testhelper, it introduces import packages loop.
    For now extract it to separate helper: `toxiproxy_test.go`.
    
    In future it would nice to move business logic code under folder `pkg`.

--- testhelper/timeout_after_test.go
+++ testhelper/timeout_after_test.go
@@ -1,17 +1,19 @@
-package testhelper
+package testhelper_test
 
 import (
 	"testing"
 	"time"
+
+	"github.com/Shopify/toxiproxy/v2/testhelper"
 )
 
 func TestTimeoutAfter(t *testing.T) {
-	err := TimeoutAfter(5*time.Millisecond, func() {})
+	err := testhelper.TimeoutAfter(5*time.Millisecond, func() {})
 	if err != nil {
 		t.Fatal("Non blocking function should not timeout.")
 	}
 
-	err = TimeoutAfter(5*time.Millisecond, func() {
+	err = testhelper.TimeoutAfter(5*time.Millisecond, func() {
 		time.Sleep(time.Second)
 	})
 	if err == nil {
